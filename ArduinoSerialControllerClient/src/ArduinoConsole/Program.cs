13717652a61ee72fb942122f2f04025f6c289ae0
samdoss <samdoss@live.com>
2019-03-10 15:38:08 +0530

    added console app

---
+++ ArduinoSerialControllerClient/src/ArduinoConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using ArduinoSerialControllerClient;
+using Samdoss.CommonLayer;
+using Samdoss.DataLayer;
+using System;
+
+namespace ArduinoConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string portName = string.Empty;
+			foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+			{
+				if (s.ToLower().Contains("com"))
+				{
+					portName = s;
+				}
+			}
+
+			if (string.IsNullOrEmpty(portName))
+			{
+				Console.WriteLine("Port Not exists");
+				return;
+			}
+
+
+			ArduinoSerialDevice device = new ArduinoSerialDevice(portName, 9600);
+			device.Connect();
+			DataStore ds = new DataStore();
+			ds.ScreenMode = ScreenMode.Add;
+			while (true)
+			{
+				string data = device.AnalogRead(3).ToString();
+				if (!string.IsNullOrEmpty(data))
+				{
+					ds.DataValue = data;
+					ds.Commit();
+					Console.WriteLine(data);
+				}
+			}
+		}
+	}
+}
